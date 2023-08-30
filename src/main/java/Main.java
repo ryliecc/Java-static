de6cbc582b511a78eecae33dde3370cb0f79a42f
Rylie Castell <rylie.castell@gmail.com>
2023-08-30 15:49:21 +0200

    add count to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Count count = new Count();
+        Count.incrementTotalCount();
+        count.incrementInstanceCount();
+        System.out.println(Count.totalCount);
+        System.out.println(count.instanceCount);
     }
+
+
+
+
 }
